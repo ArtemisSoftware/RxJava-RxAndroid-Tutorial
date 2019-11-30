716d1a7b963f862256c384371b3d009330b59264
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-11-30 17:34:44 +0000

    Create Operators - Create, Just, Range, Repeat

--- app/src/main/java/com/titan/rxjavarxandroid/MainActivity.java
+++ app/src/main/java/com/titan/rxjavarxandroid/MainActivity.java
@@ -8,11 +8,16 @@ import android.widget.TextView;
 import com.titan.rxjavarxandroid.models.Task;
 import com.titan.rxjavarxandroid.util.DataSource;
 
+import java.util.List;
+
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Function;
 import io.reactivex.functions.Predicate;
 import io.reactivex.schedulers.Schedulers;
 import timber.log.Timber;
@@ -30,44 +35,154 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         text = findViewById(R.id.text);
 
-        Observable<Task> taskObservable = Observable
-            .fromIterable(DataSource.createTasksList())
-            .subscribeOn(Schedulers.io())
-            .filter(new Predicate<Task>() {
-                @Override
-                public boolean test(Task task) throws Exception {
 
-                    Timber.d("Thread: " + Thread.currentThread().getName() + " Task complete? " + task.isComplete());
-                    return task.isComplete();
-                }
-            })
-            .observeOn(AndroidSchedulers.mainThread());
+        Observable<Integer> taskObservable = Observable
+                .range(0,9)
+                .subscribeOn(Schedulers.io())
+                .repeat(3)
+                .observeOn(AndroidSchedulers.mainThread());
 
 
-        taskObservable.subscribe(new Observer<Task>() {
+        taskObservable.subscribe(new Observer<Integer>() {
             @Override
             public void onSubscribe(Disposable d) {
-                Timber.d("onSubscribe: called");
-                disposable.add(d);
+
             }
 
             @Override
-            public void onNext(Task task) {
-                Timber.d("onNext: Thread: " + Thread.currentThread().getName());
-                Timber.d("onNext: " + task.getDescription());
-
+            public void onNext(Integer integer) {
+                Timber.d("onNext: : " + integer);
             }
 
             @Override
             public void onError(Throwable e) {
-                Timber.e("onError: " + e);
+
             }
 
             @Override
             public void onComplete() {
-                Timber.d("onComplete: called");
+
             }
         });
+    }
+
+    private void create(){
+        final Task task = new Task("Walk the dog", false, 4);
+
+        // Create the Observable
+        Observable<Task> singleTaskObservable = Observable
+                .create(new ObservableOnSubscribe<Task>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<Task> emitter) throws Exception {
+                        if(!emitter.isDisposed()){
+                            emitter.onNext(task);
+                            emitter.onComplete();
+                        }
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        // Subscribe to the Observable and get the emitted object
+        singleTaskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Task task) {
+                Timber.d("onNext: single task: " + task.getDescription());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+    }
+
+
+    private void create_2(){
+        Observable<Task> taskListObservable = Observable
+                .create(new ObservableOnSubscribe<Task>() {
+                    @Override
+                    public void subscribe(ObservableEmitter<Task> emitter) throws Exception {
+
+                        // Inside the subscribe method iterate through the list of tasks and call onNext(task)
+                        for(Task task: DataSource.createTasksList()){
+                            if(!emitter.isDisposed()){
+                                emitter.onNext(task);
+                            }
+                        }
+                        // Once the loop is complete, call the onComplete() method
+                        if(!emitter.isDisposed()){
+                            emitter.onComplete();
+                        }
+
+                    }
+                })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread());
+
+        // Subscribe to the Observable and get the emitted objects
+        taskListObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+
+            }
+
+            @Override
+            public void onNext(Task task) {
+                Timber.d("onNext: single task: " + task.getDescription());
+            }
+
+            @Override
+            public void onError(Throwable e) {
+
+            }
+
+            @Override
+            public void onComplete() {
+
+            }
+        });
+    }
+
+
+    private void just(){
+
+        Observable.just("first", "second", "third", "fourth", "fifth", "sixth",
+                "seventh", "eighth", "ninth", "tenth")
+                .subscribeOn(Schedulers.io()) // What thread to do the work on
+                .observeOn(AndroidSchedulers.mainThread()) // What thread to observe the results on
+                .subscribe(new Observer<String>() { // view the results by creating a new observer
+                    @Override
+                    public void onSubscribe(Disposable d) {
+                        Timber.d("onSubscribe: called");
+                    }
+
+                    @Override
+                    public void onNext(String s) {
+                        Timber.d("onNext: " + s);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Timber.e("onError: " + e);
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Timber.d("onComplete: done...");
+                    }
+                });
+
     }
 
     @Override
