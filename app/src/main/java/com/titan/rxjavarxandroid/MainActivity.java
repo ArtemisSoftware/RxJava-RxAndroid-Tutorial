0f3e87c654e55c9a651c01a2c1057e049b956c76
ArtemisSoftware <gustavo.isaac@gmail.com>
2019-11-30 15:47:47 +0000

    Introduction to Observables and Observers

--- app/src/main/java/com/titan/rxjavarxandroid/MainActivity.java
+++ app/src/main/java/com/titan/rxjavarxandroid/MainActivity.java
@@ -3,12 +3,71 @@ package com.titan.rxjavarxandroid;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.titan.rxjavarxandroid.models.Task;
+import com.titan.rxjavarxandroid.util.DataSource;
+
+import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.functions.Predicate;
+import io.reactivex.schedulers.Schedulers;
+import timber.log.Timber;
 
 public class MainActivity extends AppCompatActivity {
+
+    //ui
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        text = findViewById(R.id.text);
+
+        Observable<Task> taskObservable = Observable
+            .fromIterable(DataSource.createTasksList())
+            .subscribeOn(Schedulers.io())
+                .filter(new Predicate<Task>() {
+                    @Override
+                    public boolean test(Task task) throws Exception {
+
+                        Timber.d("test: Thread: " + Thread.currentThread().getName());
+
+                        try {
+                            Thread.sleep(1000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        return task.isComplete();
+                    }
+                })
+            .observeOn(AndroidSchedulers.mainThread());
+
+        taskObservable.subscribe(new Observer<Task>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Timber.d("onSubscribe: called");
+            }
+
+            @Override
+            public void onNext(Task task) {
+                Timber.d("onNext: Thread: " + Thread.currentThread().getName());
+                Timber.d("onNext: " + task.getDescription());
+
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Timber.e("onError: " + e);
+            }
+
+            @Override
+            public void onComplete() {
+                Timber.d("onComplete: called");
+            }
+        });
     }
 }
